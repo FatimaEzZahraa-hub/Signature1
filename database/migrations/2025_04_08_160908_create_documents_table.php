4ecff13026c15407b9975632dbcd6595d5911558
Fatima <fatima.2004.ski@gmail.com>
2025-04-14 13:40:26 +0100

    la 2eme modif du jour 14/04/25

--- database/migrations/2025_04_08_160908_create_documents_table.php
+++ database/migrations/2025_04_08_160908_create_documents_table.php
@@ -1,5 +1,4 @@
 <?php
-//C:\MAMP\htdocs\Signature1\database\migrations\2025_04_10_114415_create_documents_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,10 +9,11 @@ class CreateDocumentsTable extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('path');            // chemin du fichier sur le disque
-            $table->enum('status', ['en attente', 'signé', 'annulé'])
-                  ->default('en attente');
+            $table->string('titre');
+            $table->text('description')->nullable();
+            $table->string('fichier');
+            $table->date('due_date')->nullable();
+            $table->enum('status', ['en attente', 'signé', 'brouillon'])->default('brouillon');
             $table->timestamps();
         });
     }
@@ -22,4 +22,4 @@ class CreateDocumentsTable extends Migration
     {
         Schema::dropIfExists('documents');
     }
-}
+} 
